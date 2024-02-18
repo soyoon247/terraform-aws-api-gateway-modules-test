5bcda1e53706466e9f893b9a4205fb925831cd3a
soyoon <soyoon.dev@gmail.com>
2024-02-18 18:43:23 +0900

    SV-9742 aws_api_gateway_method의 request_models 추가

--- resources/main.tf
+++ resources/main.tf
@@ -6,6 +6,7 @@ resource "aws_api_gateway_method" "method" {
   authorizer_id      = var.authorizer_id
   http_method        = var.method_http_method
   request_parameters = local.method_request_parameters
+  request_models     = var.method_request_models
   operation_name     = var.method_operation_name
 }
 
