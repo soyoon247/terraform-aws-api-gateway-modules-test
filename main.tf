56e59a843c5f64c502814029580a2605216241e7
soyoon <soyoon.dev@gmail.com>
2024-06-10 14:23:44 +0900

    map 내부의 값을 참조하지 않도록 수정

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_api_gateway_resource" "resource" {
 
 locals {
   get_values = merge(lookup(var.method_values, "GET", {}), var.common_values)
-  get = {
+  get_raw_value = {
     method_http_method               = lookup(get_values, "method_http_method", null)
     api_key_required                 = lookup(get_values, "api_key_required", "false")
     authorization                    = lookup(get_values, "authorization", "NONE")
@@ -21,118 +21,127 @@ locals {
     integration_http_method          = lookup(get_values, "integration_http_method", null)
     integration_passthrough_behavior = lookup(get_values, "integration_passthrough_behavior", "WHEN_NO_MATCH")
     integration_timeout_milliseconds = lookup(get_values, "integration_timeout_milliseconds", 29000)
-    integration_type                 = lookup(get_values, "integration_type", "HTTP_PROXY")
-    integration_uri                  = lookup(get_values, "integration_uri", null)
-    integration_request_templates    = lookup(get_values, "integration_request_templates", null)
-    integration_response_map         = lookup(get_values, "integration_response_map", null)
+    # option 일 때 달라짐.
+    integration_type              = lookup(get_values, "integration_type", "HTTP_PROXY")
+    integration_uri               = lookup(get_values, "integration_uri", null)
+    integration_request_templates = lookup(get_values, "integration_request_templates", null)
+    integration_response_map      = lookup(get_values, "integration_response_map", null)
+  }
+  is_options_method         = false
+  is_proxy_type             = local.get_raw_value.integration_type == "HTTP_PROXY"
+  is_mock_type              = local.get_raw_value.integration_type == "MOCK"
+  is_http_proxy_integration = local.is_proxy_type && var.path_part == "{proxy+}"
 
-    is_options_method         = false
-    is_proxy_type             = local.get.integration_type == "HTTP_PROXY"
-    is_mock_type              = local.get.integration_type == "MOCK"
-    is_http_proxy_integration = (local.get.is_proxy_type && var.path_part == "{proxy+}")
+  _method_request_parameters_when_not_mock_type = (
+    local.get_raw_value.method_request_parameters == null && local.is_http_proxy_integration ?
+    local.proxy_method_request_parameters :
+    local.get_raw_value.method_request_parameters
+  )
 
-    _method_request_parameters_when_not_mock_type = (
-      local.get.method_request_parameters == null && local.get.is_http_proxy_integration ?
-      local.proxy_method_request_parameters :
-      local.get.method_request_parameters
+  method_request_parameters = (
+    local.is_mock_type ?
+    local.method_request_parameters :
+    (
+      local.is_proxy_type ?
+      local._method_request_parameters_when_not_mock_type :
+      merge(local._method_request_parameters_when_not_mock_type, local.x_forwarded_for_method_request_parameters)
     )
+  )
 
-    method_request_parameters = (
-      local.get.is_mock_type ?
-      local.get.method_request_parameters :
+  _method_response_map = (
+    local.get_raw_value.method_response_map != null ?
+    local.get_raw_value.method_response_map :
+    (
+      local.is_options_method ?
+      local.options_method_response_map :
       (
-        local.get.is_proxy_type ?
-        local.get._method_request_parameters_when_not_mock_type :
-        merge(local.get._method_request_parameters_when_not_mock_type, local.x_forwarded_for_method_request_parameters)
+        local.is_http_proxy_integration ?
+        local.proxy_method_response_map :
+        local.default_method_response_map
       )
     )
+  )
 
-    _method_response_map = (
-      local.get.method_response_map != null ?
-      local.get.method_response_map :
-      (
-        local.get.is_options_method ?
-        local.options_method_response_map :
-        (
-          local.get.is_http_proxy_integration ?
-          local.proxy_method_response_map :
-          local.default_method_response_map
-        )
-      )
+  integration_cache_key_parameters = (
+    local.get_raw_value.integration_cache_key_parameters != null ?
+    local.get_raw_value.integration_cache_key_parameters :
+    (
+      local.is_http_proxy_integration ?
+      local.proxy_integration_cache_key_parameters :
+      null
     )
+  )
 
-    integration_cache_key_parameters = (
-      local.get.integration_cache_key_parameters != null ?
-      local.get.integration_cache_key_parameters :
-      (
-        local.get.is_http_proxy_integration ?
-        local.proxy_integration_cache_key_parameters :
-        null
-      )
+  _integration_request_parameters_when_not_mock_type = (
+    local.get_raw_value.integration_request_parameters == null && local.is_http_proxy_integration ?
+    local.proxy_integration_request_parameters :
+    local.get_raw_value.integration_request_parameters
+  )
+
+  integration_request_parameters = (
+    local.is_mock_type ?
+    local.get_raw_value.integration_request_parameters :
+    (
+      local.is_proxy_type ?
+      local._integration_request_parameters_when_not_mock_type :
+      merge(local._integration_request_parameters_when_not_mock_type, local.x_forwarded_for_integration_request_parameters)
     )
+  )
 
-    _integration_request_parameters_when_not_mock_type = (
-      local.get.integration_request_parameters == null && local.get.is_http_proxy_integration ?
-      local.proxy_integration_request_parameters :
-      local.get.integration_request_parameters
+  integration_http_method = (
+    local.get_raw_value.integration_http_method != null ?
+    local.get_raw_value.integration_http_method :
+    (
+      local.get_raw_value.integration_type == "MOCK" ?
+      null :
+      local.get_raw_value.method_http_method
     )
+  )
 
-    integration_request_parameters = (
-      local.get.is_mock_type ?
-      local.get.integration_request_parameters :
-      (
-        local.get.is_proxy_type ?
-        local.get._integration_request_parameters_when_not_mock_type :
-        merge(local.get._integration_request_parameters_when_not_mock_type, local.x_forwarded_for_integration_request_parameters)
-      )
+  integration_request_templates = (
+    local.get_raw_value.integration_request_templates != null ?
+    local.get_raw_value.integration_request_templates :
+    (
+      local.is_options_method ?
+      local.options_integration_request_templates :
+      local.get_raw_value.integration_request_templates
     )
+  )
 
-    integration_http_method = (
-      local.get.integration_http_method != null ?
-      local.get.integration_http_method :
-      (
-        local.get.integration_type == "MOCK" ?
-        null :
-        local.get.method_http_method
-      )
-    )
-
-    integration_request_templates = (
-      local.get.integration_request_templates != null ?
-      local.get.integration_request_templates :
-      (
-        local.get.is_options_method ?
-        local.options_integration_request_templates :
-        local.get.integration_request_templates
-      )
-    )
-
-    _integration_response_map = (
-      local.get.integration_response_map != null ?
-      local.get.integration_response_map :
-      local.default_integration_response_map
-    )
+  _integration_response_map = (
+    local.get_raw_value.integration_response_map != null ?
+    local.get_raw_value.integration_response_map :
+    local.default_integration_response_map
+  )
 
 
-    method_response_map = {
-      for key, value in local.get.method_response_map : key => merge(value, { "http_method" = local.get.method_http_method })
-    }
-    integration_response_map = {
-      for key, value in local.get.integration_response_map : key => merge(value, { "http_method" = local.get.method_http_method })
-    }
-
+  method_response_map = {
+    for key, value in local._method_response_map : key => merge(value, { "method_http_method" = local.get_raw_value.method_http_method })
+  }
+  integration_response_map = {
+    for key, value in local._integration_response_map : key => merge(value, { "method_http_method" = local.get_raw_value.method_http_method })
   }
 
+
+
   value_map = {
-    "GET" = local.get
+    "GET" = merge(local.get_raw_value, {
+        "method_request_parameters" = local.method_request_parameters,
+        "method_response_map"       = local.method_response_map,
+        "integration_cache_key_parameters" = local.integration_cache_key_parameters,
+        "integration_request_parameters"   = local.integration_request_parameters,
+        "integration_http_method"          = local.integration_http_method,
+        "integration_request_templates"    = local.integration_request_templates,
+        "integration_response_map"         = local.integration_response_map
+    })
   }
 
   integration_response_map_list = flatten(
-    local.get.integration_response_map
+    local.integration_response_map
   )
 
   method_response_map_list = flatten(
-    local.get.method_response_map
+    local.method_response_map
   )
 }
 
